8a03d4b3e7a0553cbe6e4e5063df784ef65e8961
oussenimbohou <oussenimbohou@gmail.com>
2023-10-17 20:37:32 +0100

    Remove_Duplicate_From_Sorted_Array Version 3 solution

--- Solution.java
+++ Solution.java
@@ -1,15 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] numbers = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println("Remove Duplicate " + sol.removeDuplicatesV1(numbers));
+        int[] numbers = {0,0,0,0};
+        System.out.println("Remove Duplicate " + sol.removeDuplicatesV3(numbers));
 
     }
     public int removeDuplicates(int[] nums) {
@@ -32,4 +29,55 @@ public class Solution {
         System.out.println("List = " + list);
         return list.size();
     }
+    public int removeDuplicatesV2(int[] nums){
+        int current = 0;
+        int temp = 1;
+        int counter = 0;
+
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return 1;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != '\0') break;
+            if(i == nums.length - 1) return 1;
+        }
+        while(current < nums.length && counter < nums.length){
+           if(nums[current] == nums[temp]){
+               rightShift(nums, temp);
+           } else{
+               temp++;
+               current++;
+               if(temp == nums.length) break;
+           }
+           counter++;
+           System.out.println("current = "+current+" temp = "+temp);
+        }
+        System.out.println("nums = " + Arrays.toString(nums));
+        int count = 0;
+        for (int i = nums.length - 1; i >= 0 ; i--) {
+            if(nums[i] == '\0') count++;
+            if(nums[i] != '\0') break;
+        }
+        return nums.length - count;
+    }
+
+    private static void rightShift(int[] nums, int temp) {
+        int i;
+        for ( i = temp; i < nums.length ; i++) {
+            if (i < nums.length - 1) nums[i] = nums[i + 1];
+            else break;
+        }
+        nums[i] = '\0';
+        System.out.println("nums = " + Arrays.toString(nums) + ", temp = " + temp);
+    }
+    public int removeDuplicatesV3(int[] nums){
+        int k = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != nums[k-1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
 }
